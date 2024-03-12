5bed78aa32d81666cc9e9dfa68aa7d466fa926fc
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-03-12 15:14:04 -0700

    Init express and add user model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: { type: String, required: true, minLength: 1, maxLength: 100, },
+    lastName: { type: String, required: true, minLength: 1, maxLength: 100 },
+    username: { type: String, required: true, minLength: 1, maxLength: 50 },
+    password: { type: String, required: true, minLength: 1 }
+});
+
+UserSchema.virtual("fullName").get(function() {
+    let fullName = "";
+    if (this.firstName && this.lastName) {
+        fullName = `${this.firstName} ${this.lastName}`;
+    }
+
+    return fullName;
+});
